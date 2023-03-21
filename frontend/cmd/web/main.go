30a1d74fc87dbeceae7ab1f40c6df2602210adfd
igorakimy <igorakimy@gmail.com>
2023-03-21 12:00:00 +0300

    fix(frontend): move broker url and frontend port to env file
    Add sending broker url to template for using in scripts and move broker url to env variable

--- frontend/cmd/web/main.go
+++ frontend/cmd/web/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+)
+
+var (
+	webPort = os.Getenv("FRONTEND_APP_PORT")
 )
 
 func main() {
@@ -12,36 +18,45 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting frontend service on port 80")
-	err := http.ListenAndServe(":80", nil)
+	fmt.Printf("Starting frontend service on port %s", webPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", webPort), nil)
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
+//go:embed templates
+var templateFS embed.FS
+
 // render builds template from layout files and execute it.
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if err := tmpl.Execute(w, nil); err != nil {
+	var data struct {
+		BrokerURL string
+	}
+
+	data.BrokerURL = os.Getenv("BROKER_SERVICE_URL")
+
+	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
